878159e99776045f99eba2193b5c8c70840e1279
Ariel29123 <73623027+Ariel29123@users.noreply.github.com>
2021-08-17 22:09:56 -0300

    Update Ej8.c

--- EjAlgTD/Ej8.c
+++ EjAlgTD/Ej8.c
@@ -1,3 +1,8 @@
+/*
+Escribir un programa que acepte tres argumentos de tipo double: x0, v0 y t.
+Imprimir el resultado de x0 + v0t + gt2/2. 
+O sea la ecuación de desplazamiento para un tiro vertical después de t segundos, con posición inicial x0 y velocidad inicial v0.
+*/
 #include<stdio.h>
 #include<stdlib.h>
 
