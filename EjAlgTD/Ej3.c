d4b54d894721935cb23439e140e700d4862d509a
Ariel29123 <73623027+Ariel29123@users.noreply.github.com>
2021-08-17 22:07:10 -0300

    Update Ej3.c

--- EjAlgTD/Ej3.c
+++ EjAlgTD/Ej3.c
@@ -1,3 +1,6 @@
+/*
+Escribir un programa que tome tres argumentos enteros positivos e imprima "falso" si alguno es igual o mayor a la suma de los otros dos, o "verdadero" en caso contrario.
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
