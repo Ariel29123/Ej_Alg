26426a56f1031a6e45e782420ec56d516e7e2865
Ariel29123 <73623027+Ariel29123@users.noreply.github.com>
2021-08-17 22:14:24 -0300

    Add files via upload

---
+++ EjAlgTD/Ej9.c
@@ -0,0 +1,23 @@
+/*
+Escribir un programa que acepte dos argumentos: día y mes (ambos de tipo int). Imprimir "verdadero" si la fecha está entre el 20 de marzo y el 20 de junio, o "falso" de lo contrario.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int int main(int argc, char const *argv[]) {
+  int dia = atoi(argv[1]);
+  int mes = atoi(argv[2]);
+
+  printf("Fecha: %d\n", dia, mes );
+
+  bool entre_fechas = false;
+
+  entre_fechas = (mes >= 3) && (mes <= 6);
+  entre_fechas = entre_fechas || (mes >= 4 || mes <= 5);
+  entre_fechas = entre_fechas || (mes >= 6 || dia <= 20);
+
+  printf("%s\n", entre_fechas ? "verdadero" : "falso" );
+
+  return 0;
+}
