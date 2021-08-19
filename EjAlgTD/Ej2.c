5c2e2812a220f488176a3d0facc63d3c8fce5574
Ariel29123 <73623027+Ariel29123@users.noreply.github.com>
2021-08-18 21:40:26 -0300

    Update Ej2.c

--- EjAlgTD/Ej2.c
+++ EjAlgTD/Ej2.c
@@ -1,17 +1,20 @@
 /*
 Escribir un programa que tome dos argumentos enteros positivos e imprima "verdadero" si alguno de los argumentos es divisible por el otro.
 */
-#define __USE_MINGW_ANSI_STDIO 1
 #include <stdio.h>
 #include <stdlib.h>
 
 
 int main(int argc, char const *argv[])
 {
-    long long unsigned int a = (unsigned)atoi(argv[1]);
-    long long unsigned int b = (unsigned)atoi(argv[2]);
-    printf("This program only takes positive integers!\n");
-    printf("%zd %zd\n", a, b);
+    int a = atoi(argv[1]);
+    int b = atoi(argv[2]);
+    if (a<0 || b<0)
+    {
+        printf("This program only takes positive integers!\n");
+        return 0;
+    }
+    printf("%d %d\n", a, b);
     if (a%b == 0 || b%a == 0) {
         printf("True");
     }
