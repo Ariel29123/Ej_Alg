4988a6980fd54ea43373e28b345e5da74824ddf6
Ariel29123 <73623027+Ariel29123@users.noreply.github.com>
2021-08-18 21:56:25 -0300

    Update Ej8.c

--- EjAlgTD/Ej8.c
+++ EjAlgTD/Ej8.c
@@ -3,18 +3,15 @@ Escribir un programa que acepte tres argumentos de tipo double: x0, v0 y t.
 Imprimir el resultado de x0 + v0t + gt2/2. 
 O sea la ecuación de desplazamiento para un tiro vertical después de t segundos, con posición inicial x0 y velocidad inicial v0.
 */
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int int main(int argc, char *argv[]) {
-  int n = atoi(argv[1]);
-
-  for (int i = 0-;i < n; i++) {
-   for (int j = 0; j < n; j++) {
-     if (i + j = 2 ==0)
-      printf("1", );
-    else
-     printf("0 " );
-  }
-  printf("\n");
+int main(int argc, char const *argv[])
+{
+    printf("This program only takes numbers!\n");
+    double xi = atof(argv[1]);
+    double vi = atof(argv[2]);
+    double t = atof(argv[3]);
+    printf("%f", xi + vi*t + (9.807*(t*t)) / 2);
+    return 0;
 }
