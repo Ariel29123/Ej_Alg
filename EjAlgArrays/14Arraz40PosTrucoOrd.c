6d5c959aa5baac438531a9fa5949b5925ab3d613
Ariel29123 <73623027+Ariel29123@users.noreply.github.com>
2021-10-27 20:14:10 -0300

    Update 14Arraz40PosTrucoOrd.c

--- EjAlgArrays/14Arraz40PosTrucoOrd.c
+++ EjAlgArrays/14Arraz40PosTrucoOrd.c
@@ -7,24 +7,18 @@ int main(void)
 {
     char *type[4]= {"copas", "basto", "espada", "oro"};
     char *number[10]= {"Uno", "Dos", "Tres", "Cuatro", "Cinco", "Seis", "Siete", "Diez", "Once", "Doce"};
-    char *deck[40];
+    char deck[40][100];
     int deckIndex= 0;
     for (int i = 0; i < 4; i++)
     {
         for (int j = 0; j < 10; j++) {
-          char card[100] = "";
-          strcat(card, number[j]);
-          strcat(card, " de ");
-          strcat(card, type[i]);
-          strcat(card, "\n");
-          printf("%s\n", card);
-          deck[(i*10) + j]= card;
+          sprintf(deck[deckIndex], "%s de %s\n", number[j], type[i]);
           deckIndex++;
         }
     }
     for (int i = 0; i < 40; i++)
     {
-        // printf("%s\n", deck[i]);
+        printf(deck[i]);
     }
     return 0;
 }
